2b9df4d6b533966c18070f14669ed14fe1325aa5
Alexander Peregontsev <110561969+APeregontsev@users.noreply.github.com>
2023-07-24 00:12:37 +0300

    Delete src directory

--- src/UI/ModalSubTitle/ModalSubTitle.tsx
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-const ModalSubTitle = ({ children }: { children: string }) => {
-  return <div className="inner-modal-title">{children}</div>;
-};
-
-export default ModalSubTitle;
